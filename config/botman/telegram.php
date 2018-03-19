d63a1946b476cfdf72b33fe4ce559d3424dccd5e
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2018-03-19 22:33:46 +0800

    first commit

---
+++ config/botman/telegram.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Telegram Token
+    |--------------------------------------------------------------------------
+    |
+    | Your Telegram bot token you received after creating
+    | the chatbot through Telegram.
+    |
+    */
+    'token' => env('596051890:AAFAmwd3kKHh5G6X-B4evc56l-g-o7KkrGw'),
+];
